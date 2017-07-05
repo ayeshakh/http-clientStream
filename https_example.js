67eb8e92b5ca9b8a5aae17bf8999f67b85811ea1
Ayesha <ayesha_04@yahoo.com>
2017-07-05 03:10:23 +0000

    third  commit

--- https_example.js
+++ https_example.js
@@ -11,9 +11,18 @@ var options = {
 var callback = function() {
   console.log('In response handler callback!');
 }
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
+}
 
 console.log("I'm about to make the request!");
 
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
+}
